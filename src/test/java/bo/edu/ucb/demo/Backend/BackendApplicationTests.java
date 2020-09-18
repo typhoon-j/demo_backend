41e6eedb09fee5bf1c68afde6802c0c7152db949
Ernesto Campohermoso <ecampohermoso@cirrus-it.net>
2020-09-17 20:12:08 -0400

    first commit

---
+++ src/test/java/bo/edu/ucb/demo/Backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.demo.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
